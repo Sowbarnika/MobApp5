4f6bd3e0310e04adfafcc52c9cefaa226351c08d
ITLAB <ITLAB@IT217>
2017-02-20 14:10:35 +0530

    Fifth

--- src/com/example/sdcard/MainActivity.java
+++ src/com/example/sdcard/MainActivity.java
@@ -6,6 +6,8 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -15,28 +17,31 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-     Button b1=(Button)findViewById(R.id.button1);
-     b1.setOnClickListener(new View.OnClickListener(){
+        final EditText eText = (EditText) findViewById(R.id.editText1);
+        Button b1=(Button)findViewById(R.id.button1);
+        b1.setOnClickListener(new View.OnClickListener(){
 
-		@Override
-		public void onClick(View v) {
-			// TODO Auto-generated method stub
+        	@Override
+        	public void onClick(View v) {
+        		// TODO Auto-generated method stub
 			
 			
-		}
+        	}
     	 
-     });
+        });
   
-     Button b2=(Button)findViewById(R.id.button2);
-     b2.setOnClickListener(new View.OnClickListener(){
+        Button b2=(Button)findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener(){
 
-		@Override
-		public void onClick(View arg0) {
-			// TODO Auto-generated method stub
-			
-		}
+        	@Override
+        	public void onClick(View arg0) {
+        		// TODO Auto-generated method stub
+        		String str = eText.getText().toString();
+                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
+                msg.show();
+        	}
     	 
-     });
-    }
+        });
+    	}
   
-}
+	}
