50a055a78d95758d2f0ea85f50822c9c8c89e155
ITLAB <ITLAB@IT217>
2017-02-20 15:16:26 +0530

    Sixth

--- src/com/example/sdcard/MainActivity.java
+++ src/com/example/sdcard/MainActivity.java
@@ -1,12 +1,14 @@
 package com.example.sdcard;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -17,6 +19,7 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final TextView tv=(TextView)findViewById(R.id.textView1);
         final EditText eText = (EditText) findViewById(R.id.editText1);
         Button b1=(Button)findViewById(R.id.button1);
         b1.setOnClickListener(new View.OnClickListener(){
@@ -37,11 +40,21 @@ public class MainActivity extends Activity {
         	public void onClick(View arg0) {
         		// TODO Auto-generated method stub
         		String str = eText.getText().toString();
-                Toast msg = Toast.makeText(getBaseContext(),str,Toast.LENGTH_LONG);
-                msg.show();
+        		try {
+                    FileInputStream fin = openFileInput("SDCard.txt");
+                    int c;
+                    String temp="";
+                    while( (c = fin.read()) != -1){
+                       temp = temp + Character.toString((char)c);
+                    }
+                    tv.setText(temp);
+                    Toast.makeText(getBaseContext(),"file read",Toast.LENGTH_SHORT).show();
+                 }
+                 catch(Exception e){
+                 }
         	}
     	 
         });
     	}
-  
-	}
+}
+	
